da7a1ec40eb2b24fd510a0270855addba6c5466f
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-07 11:17:27 +0300

    Selenium D07/P01

---
+++ src/test/java/day07_assertion_dropdown_testbase/C01_Assertion.java
@@ -0,0 +1,13 @@
+package day07_assertion_dropdown_testbase;
+
+public class C01_Assertion {
+
+    /*
+    Assertion, bir test sırasında beklenen (expected) ve gerçek (actual) sonuçlar arasındaki uyumu doğrulamak için kullanılır
+    Eğer beklenen sonuç gerçekleşen sonuç ile uyuşmuyorsa assertion exception fırlatılır ve test failed olur.
+    Junit'te bir assertion failed olduğunda o anda çalışmakta olan test methodu durdurulur ve bir assertion error fırlatılır.
+    Bu, ilgili testin başarısız olduğu anlamına gelir.
+     */
+
+
+}
