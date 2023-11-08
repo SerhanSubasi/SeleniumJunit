28214ffe1dc1e99d61d47ccb16d74edbc98dfa35
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-08 23:54:35 +0300

    Selenium ödev

--- src/test/java/day08_testbase_alert_iframe/ÖDEV.java
+++ src/test/java/day08_testbase_alert_iframe/ÖDEV.java
@@ -1,8 +1,22 @@
 package day08_testbase_alert_iframe;
 
-public class ÖDEV {
-    //https://html.com/tags/iframe/ sayfasina gidiniz
-    // sayfada bulunan youtube videosunu baslatiniz
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
 
+public class ÖDEV extends TestBase {
+
+
+
+
+    @Test
+    public void test() {
+        //https://html.com/tags/iframe/ sayfasina gidiniz
+        driver.get("https://html.com/tags/iframe/");
+        //sayfada bulunan youtube videosunu baslatiniz
+        driver.switchTo().frame(0);
+        driver.findElement(By.xpath("//div[@class='ytp-cued-thumbnail-overlay']")).click();
+        waitForSecond(5);
+    }
 
 }
