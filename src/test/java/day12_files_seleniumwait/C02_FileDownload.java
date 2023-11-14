e5082bc7092566c37ddb59455799ce2f58215d4a
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-14 12:46:10 +0300

    Selenium day12-part02

--- src/test/java/day12_files_seleniumwait/C02_FileDownload.java
+++ src/test/java/day12_files_seleniumwait/C02_FileDownload.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import utilities.TestBase;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -12,52 +13,74 @@ public class C02_FileDownload extends TestBase {
 
     @Test
     public void downloadTest() {
+
         //https://testcenter.techproeducation.com/index.php?page=file-download adresine gidelim.
         driver.get("https://testcenter.techproeducation.com/index.php?page=file-download");
+
         //b10 all test cases dosyasını indirelim
-         driver.findElement(By.linkText("b10 all test cases, code.docx")).click();
+        driver.findElement(By.partialLinkText("b10 all test cases, code")).click();
+
+        waitForSecond(5);
+
+
         //Dosyanın başarıyla indirilip indirilmediğini test edelim
-        // C:\Users\Anduril\Downloads\b10 all test cases, code.docx
+
+
+        //Dynamic dosya yolu almanin birinci yolu
+        //C:\Users\mua       \Downloads\b10 all test cases, code.docx
         String dosyaYolu = System.getProperty("user.home") + "\\Downloads\\b10 all test cases, code.docx";
 
-        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
-
-        // 2. yol
+        //Dynamic dosya yolu almanin ikinci yolu
         String userHome = System.getProperty("user.home");
-        String ortakYol = "\\Downloads\\b10 all test cases, code.docx";
-        String dynamicPath = userHome + ortakYol;
-        boolean isExist = Files.exists(Paths.get(dynamicPath));
-        Assert.assertTrue(isExist);
+        String herkesteAyniOlanYol="\\Downloads\\b10 all test cases, code.docx";
+        String dynamicPath =userHome+herkesteAyniOlanYol;
+
+
+        Assert.assertTrue( Files.exists(Paths.get(dosyaYolu)));
+
 
     }
+
 
     @Test
-    public void downloadDynamicTest() {
-
-
+    public void downloadDynamicTest()  {
+        //Dynamic dosya yolu almanin ikinci yolu
         String userHome = System.getProperty("user.home");
-        String ortakYol = "\\Downloads\\b10 all test cases, code.docx";
-        String dynamicPath = userHome + ortakYol;
-        Files.delete(Paths.get(dynamicPath));   //Dosya varsa siler ki tekrar çalıştıracağımızda hata olmasın.
+        String herkesteAyniOlanYol="\\Downloads\\b10 all test cases, code.docx";
+        String dynamicPath =userHome+herkesteAyniOlanYol;
 
-        boolean isExist = Files.exists(Paths.get(dynamicPath));
-        Assert.assertTrue(isExist);
+        //Bu kod verilen dosya yolundaki dosyayi siler, Eger belirtilen yolda bir dosya varsa siler,
+        // yoksa veya herhangi bir sebepten dosya silinemz ise bir exception firlatir
 
+
+        try {
+            Files.delete(Paths.get(dynamicPath));
+        } catch (IOException e) {
+            System.out.println("Belirtilen dosya silinemedi !!!! ");
+        }
 
 
         //https://testcenter.techproeducation.com/index.php?page=file-download adresine gidelim.
         driver.get("https://testcenter.techproeducation.com/index.php?page=file-download");
+
         //b10 all test cases dosyasını indirelim
-        driver.findElement(By.linkText("b10 all test cases, code.docx")).click();
+        driver.findElement(By.partialLinkText("b10 all test cases, code")).click();
+
+        waitForSecond(5);
 
 
         //Dosyanın başarıyla indirilip indirilmediğini test edelim
-        // C:\Users\Anduril\Downloads\b10 all test cases, code.docx
-        String dosyaYolu = System.getProperty("user.home") + "\\Downloads\\b10 all test cases, code.docx";
 
-        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+        //Dynamic dosya yolu almanin birinci yolu
+        //C:\Users\mua       \Downloads\b10 all test cases, code.docx
+        String dosyaYolu = System.getProperty("user.home") + "\\Downloads\\b10 all test cases, code.docx";
+        Assert.assertTrue( Files.exists(Paths.get(dosyaYolu)));
 
 
     }
+
+
+
 
 }
