2b4ae9050f88002b414c5f515f6769ac14fbd497
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-18 18:53:33 +0300

    Selenium calisma

--- src/test/java/day14_exception_screenshoot/C02_Screenshot.java
+++ src/test/java/day14_exception_screenshoot/C02_Screenshot.java
@@ -1,13 +1,19 @@
 package day14_exception_screenshoot;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -66,5 +72,30 @@ public class C02_Screenshot extends TestBase {
         String dosyaYolu = "C:\\Users\\Anduril\\IdeaProjects\\B189SeleniumJunitDT\\src\\test\\java\\screenShots\\"+date+"facebook.png";
         TakesScreenshot ts = (TakesScreenshot) driver;
         Files.write(Paths.get(dosyaYolu),ts.getScreenshotAs(OutputType.BYTES));
+    }
+
+    @Test
+    public void testim() throws IOException {
+        driver.get("https://mangasehri.net/manga/one-piece/bolum-165/");
+        Actions actions = new Actions(driver);
+        WebElement sonraki = driver.findElement(By.xpath("(//a[@class='btn next_page'])[3]"));
+        actions.moveToElement(sonraki);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(sonraki));
+        String dosyaYolum = "C:\\Users\\Anduril\\IdeaProjects\\B189SeleniumJunitDT\\src\\test\\java\\screenShots\\OnePiece.png";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        Files.write(Paths.get(dosyaYolum),ts.getScreenshotAs(OutputType.BYTES));
+    }
+
+    @Test
+    public void testim2() throws IOException {
+        driver.get("https://twitter.com/home");
+        WebElement ileri = driver.findElement(By.xpath("(//div[@role='button'])[3]"));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(ileri));
+        String dosyaYolu = "C:\\Users\\Anduril\\IdeaProjects\\B189SeleniumJunitDT\\src\\test\\java\\screenShots\\twitter.png";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        Files.write(Paths.get(dosyaYolu),ts.getScreenshotAs(OutputType.BYTES));
+
     }
 }
