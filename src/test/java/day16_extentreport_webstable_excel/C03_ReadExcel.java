494bf93c471c66bf96321f988af4fd746750e20d
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-20 13:56:09 +0300

    Selenium day16-part03

---
+++ src/test/java/day16_extentreport_webstable_excel/C03_ReadExcel.java
@@ -0,0 +1,17 @@
+package day16_extentreport_webstable_excel;
+
+import org.junit.Test;
+
+public class C03_ReadExcel {
+
+    /*
+    1- Framework'ümüzde excel dosyaları ile çalışabilmek için gerekli dependencyleri pom excemel'e yükleriz.
+    2- Projemize eklediğimiz excel dosyasının yolunu almalıyız. -Path-
+     */
+
+    @Test
+    public void test01() {
+        String path = "src/test/java/resources/Capitals.xlsx";
+    }
+
+}
