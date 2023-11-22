6ee80e8f96e41ab627b8ad1931e1524a94b565b3
Serhan Subaşı <subasiserhan@gmail.com>
2023-11-22 11:50:29 +0300

    Selenium day18-part01

---
+++ src/test/java/day18_jsexecutor_log4j/C01_JsExecutorClick.java
@@ -0,0 +1,31 @@
+package day18_jsexecutor_log4j;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C01_JsExecutorClick extends TestBase {
+
+    /*
+            ElementClickInterceptedException: Locate'ini almış olduğumuz bir web elementin önünde başka bir web element açılırsa,
+            click yapılamıyor ve bu hatayı veriyor.
+             */
+
+    @Test
+    public void test01() {
+        //amazona gidelim
+        driver.get("https://amazon.com");
+
+        //sell linkine tıklayalım
+        WebElement sellLinki = driver.findElement(By.xpath("//*[.='Sell']"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();",sellLinki);
+
+
+
+    }
+}
